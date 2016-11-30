e91e1d540e8b9880f3f786f732f824c3376f6600
Raphael Brugier <raphael.brugier@gmail.com>
2016-11-30 15:21:40 -0500

    add second and third queries

--- src/main/scala/com/github/rbrugier/MainSteps.scala
+++ src/main/scala/com/github/rbrugier/MainSteps.scala
@@ -3,6 +3,7 @@ package com.github.rbrugier
 import com.mongodb.spark.sql._
 import com.typesafe.scalalogging.slf4j.LazyLogging
 import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.functions.{max, min}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object MainSteps extends App with LazyLogging {
@@ -19,12 +20,42 @@ object MainSteps extends App with LazyLogging {
   zipDf.printSchema() // 3)
   zipDf.show()
 
+  // Query 1
   println( "States with Populations above 10 Million" )
+  import sqlContext.implicits._ // 1)
   zipDf.groupBy("state")
     .sum("pop")
     .withColumnRenamed("sum(pop)", "count")
-    .filter(zipDf("count") > 10000000)
+    .filter($"count" > 10000000)
     .show()
 
+  // Query 2
+  println( "Average City Population by State" )
+  zipDf
+    .groupBy("state", "city")
+    .sum("pop")
+    .withColumnRenamed("sum(pop)", "count")
+    .groupBy("state")
+    .avg("count")
+    .withColumnRenamed("avg(count)", "avgCityPop")
+    .show()
 
+  // Query 3
+  println( "Largest and Smallest Cities by State" )
+  val popByCity = zipDf // 1)
+    .groupBy("state", "city")
+    .sum("pop")
+    .withColumnRenamed("sum(pop)", "count")
+
+  popByCity.join(
+    popByCity
+      .groupBy("state")
+      .agg(max("count") as "max_pop", min("count") as "min_pop") // 2)
+      .withColumnRenamed("state", "r_state"),
+    $"state" === $"r_state" && ( $"count" === $"max_pop" || $"count" === $"min_pop") // 3)
+  )
+    .drop($"r_state")
+    .drop($"max_pop")
+    .drop($"min_pop") // 4)
+    .show()
 }
