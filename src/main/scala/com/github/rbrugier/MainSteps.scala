9a3f197e43dac5de547bf1704e0bba264a0bc68d
Raphael Brugier <raphael.brugier@gmail.com>
2016-11-30 14:01:19 -0500

    read data from mongodb section

---
+++ src/main/scala/com/github/rbrugier/MainSteps.scala
@@ -0,0 +1,21 @@
+package com.github.rbrugier
+
+import com.mongodb.spark.sql._
+import com.typesafe.scalalogging.slf4j.LazyLogging
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+object MainSteps extends App with LazyLogging {
+
+  val conf = new SparkConf()
+    .setAppName("mongozips")
+    .setMaster("local[*]")
+    .set("spark.mongodb.input.uri", "mongodb://127.0.0.1/test.zips") // 1)
+
+  val sc = new SparkContext(conf)
+  val sqlContext = new SQLContext(sc)
+  val zipDf = sqlContext.read.mongo() // 2)
+
+  zipDf.printSchema() // 3)
+  zipDf.show()
+}
